2a40f8734462053cec6272719f32f1ba1fe18e9c
GIAN PELISSER <33161429+gianpelisser@users.noreply.github.com>
2022-03-02 16:36:04 -0300

    Update config.py

--- discord_dashboard/config.py
+++ discord_dashboard/config.py
@@ -4,14 +4,14 @@ avatar_base_url = 'https://cdn.discordapp.com/'  # Nao altere
 client_id = ''  # ID do BOT (app criado no discord)
 client_secret = ''  # Secret do app criado no bot, nao é o token do bot
 client_scope = "identify guilds guilds.members.read"  # Scope, Pedir o q p/ usuario autorizar. Default=identify
-redirect_url = 'http://localhost:5000/callback/'
+redirect_url = 'http://localhost:5000/callback'
 client_token = ''
 client_url_login = f"https://discord.com/api/oauth2/authorize?client_id={client_id}&redirect_uri={redirect_url}&response_type=code&scope={client_scope}"
 ch_historico = 941899483837509672
 id_servidor_sas = 605389794641707018  # SAS - The Isle
 
 # Config MySQL
-servidor_host = "192.168.1.2"  # Endereco IP/domino do Banco de Dados
+servidor_host = "127.0.0.1"  # Endereco IP/domino do Banco de Dados
 servidor_user = "user"  # usuario do Banco de dados
 servidor_mydb = "DB"  # Nome da database do Banco de dados
 servidor_pass = "pass"  # Senha do Banco de dados
