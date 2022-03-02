0d73900432b8cca2c2043f445c89ee65e14120fe
GIAN PELISSER <33161429+gianpelisser@users.noreply.github.com>
2022-03-02 16:44:06 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ app = create_app()
 
 if __name__ == '__main__':
     # app.run(debug=True, host='0.0.0.0', port=5008, ssl_context=('ssl/server.csr', 'ssl/server.key'))
-    app.run(debug=True, host='0.0.0.0', port=5008)
+    app.run(debug=True, port=5000)
